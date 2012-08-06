64d0e12900646717f4d2f84b197257d933e6588a
andrew bueno <abueno@andrew.cmu.edu>
2012-08-06 16:11:56 -0400

    Changed SubtitleUnit from a struct to a class, and added functions to SubtitleUnit for setting its various attributes. Also made each SubtitleUnit have a hard limit of two lines of text. Made some quick changes to ensure that all strings are now UTF8Strings as per ofxUnicode.

--- src/ofxSubtitles.cpp
+++ src/ofxSubtitles.cpp
@@ -8,10 +8,100 @@
 
 #include <iostream>
 
-ofxSubtitles::ofxSubtitles(string subPath, string fontPath, int fps){
+/*----------------------------------------------------------------------------
+ 
+                                SubtitleUnit 
+ 
+ ---------------------------------------------------------------------------*/
+
+void SubtitleUnit::setIndex(int i){
+    index = i;
+}
+
+
+void SubtitleUnit::getIndex(){
+    return index;
+}
+
+
+void SubtitleUnit::setStartTime(long millis){
+    startTime = millis;
+}
+
+
+long SubtitleUnit::getStartTime(){
+    return startTime;
+}
+
+
+void SubtitleUnit::setEndTime(long millis){
+    endTime = millis;
+}
+
+
+long SubtitleUnit::getEndTime(){
+    return endTime;
+}
+
+
+bool SubtitleUnit::setTitles(vector<ofUTF8String> phrases){
+    if(phrases.size() < 3){
+        text = words;
+        return true;
+    }
+    else return false;
+}
+
+
+bool SubtitleUnit::addTitle(ofUTF8String phrase){
+    if(text.size() < 2){
+        text.push_back(phrase);
+        return true;
+    }
+    else return false;
+}
+
+
+UTF8String SubtitleUnit::removeTitle(int i){
+    if(i > (text.size() - 1) || i < 0){
+        ofLogError("Title index out of bounds.");
+        return NULL;
+    }
+    else{
+        UTF8String removedTitle = text[i];
+        text.erase(i);
+        return removedTitle;
+    }
+}
+
+
+vector<UTF8String> SubtitleUnit::getText(){
+    return text;
+}
+
+
+void SubtitleUnit::print(){
+    cout << index << endl;
+    cout << startTime << " --> " << endTime << endl;
+    
+    for(int i = 0; i < text.size(); i++){
+        cout << text[i] << endl;
+        cout << endl;
+    }
+    
+    cout << endl;
+}
+
+/*----------------------------------------------------------------------------
+ 
+                                ofxSubtitles 
+ 
+ ---------------------------------------------------------------------------*/
+
+ofxSubtitles::ofxSubtitles(string subPath, string fontPath, int fontSize, int fps){
     subsLoaded = loadSubs(subPath);
-    fontLoaded = loadFont(fontPath);
-    setFPS(fps);
+    font.loadFont(fontPath, fontSize);
+    setFramesPerSecond(fps);
 }
 
 ofxSubtitles::~ofxSubtitles(){
@@ -20,33 +110,33 @@ ofxSubtitles::~ofxSubtitles(){
 
 
 bool ofxSubtitles::loadSubs(string path){
-    subtitles = ofBufferFromFile(path);
+    srtFile = ofBufferFromFile(path);
     
-    if(subtitles = NULL || (path.substr(path.length - 4, 4).compare(".srt") != 0)){
+    if(srtFile = NULL || (path.substr(path.length - 4, 4).compare(".srt") != 0)){
         ofLogError("Invalid subtitle file path. Check to see if you misspelled the path, that the file does not exist, or that the file is not a .srt");
         subsLoaded = false;
     }
     else{
-        ofUTF8String line = subtitles.getFirstLine();
-        while(!subtitles.isLastLine()){
-            Subtitle title;
-            title->index = int(line);
+        ofUTF8String line = srtFile.getFirstLine();
+        while(!srtFile.isLastLine()){
+            SubtitleUnit title;
+            title.setIndex = int(line);
             
             //Fill the new subtitle struct with its start and end times
-            line = subtitles.getNextLine();
+            line = srtFile.getNextLine();
             vector<string> times = ofSplitString(line, " ");
-            Subtitle->startTime = timecode.millisForMSTimecode(times[0]);
-            Subtitle->endTime = timecode.millisForMSTimecode(times[2]);
-            line = subtitles.getNextLine();
+            title.setStartTime(timecode.millisForMSTimecode(times[0]));
+            title.setEndTime(timecode.millisForMSTimecode(times[2]));
+            line = srtFile.getNextLine();
             
             //Add each spoken line to the subtitle struct
             while(line.compare("") == 0){
-                title->text.push_back(line);
-                line = subtitles.getNextLine();
+                title.addTitle(line);
+                line = srtFile.getNextLine();
             }
             
-            subs.push_back(title);
-            line = subtitles.getNextLine(); //This should be the number of the next subtitle in the .srt,
+            subtitleList.push_back(title);
+            line = srtFile.getNextLine(); //This should be the number of the next subtitle in the .srt,
                                             //or empty if the end of the buffer has been reached.
         }
         
@@ -55,32 +145,32 @@ bool ofxSubtitles::loadSubs(string path){
 }
 
 
-void ofxSubtitles::loadFont(string path, int fontsize){
-    font.loadFont(path, fontsize);
-    
-    if(!font.isLoaded()){
-        ofLogError("Invalid font file path. Check to see if you misspelled the path, that the file does not exist, or that the file is not a .srt");
+//void ofxSubtitles::loadFont(string path, int fontsize){
+//    font.loadFont(path, fontsize);
+//    
+//    if(!font.isLoaded()){
+//        ofLogError("Invalid font file path. Check to see if you misspelled the path, that the file does not exist, or that the file is not a .srt");
+//
+//        fontLoaded = false;
+//    }
+//    else subsLoaded = true;
+//}
+//
+//
+//bool ofxSubtitles::loadFont(string path, int fontsize, bool antiAliased, bool fullCharacterSet, 
+//                            bool makeContours, bool simplifyAmt, int dpi){
+//    
+//    font.loadFont(path, fontsize, antiAliased, fullCharacterSet, makeContours, 
+//                  simplifyAmt, dpi);
+//    
+//    if(!font.isLoaded()){
+//        ofLogError("Invalid font file path. Check to see if you misspelled the path, or that the file does not exist.");
+//        return false;
+//    }
+//    else return true;
+//}
 
-        fontLoaded = false;
-    }
-    else subsLoaded = true;
-}
-
-
-bool ofxSubtitles::loadFont(string path, int fontsize, bool antiAliased, bool fullCharacterSet, 
-                            bool makeContours, bool simplifyAmt, int dpi){
-    
-    font.loadFont(path, fontsize, antiAliased, fullCharacterSet, makeContours, 
-                  simplifyAmt, dpi);
-    
-    if(!font.isLoaded()){
-        ofLogError("Invalid font file path. Check to see if you misspelled the path, or that the file does not exist.");
-        return false;
-    }
-    else return true;
-}
-
-void ofxSubtitles::setFPS(int fps){
+void ofxSubtitles::setFramesPerSecond(int fps){
     timecode.setFPS(fps);
 }
        
--- src/ofxSubtitles.h
+++ src/ofxSubtitles.h
@@ -15,60 +15,83 @@
 #include "ofFileUtils.h"
 #include "ofxUnicode.h"
 
+#define MAX_LINES_PER_SUBTITLE_UNIT 2
+
+class SubtitleUnit{
+
+public:
+    SubtitleUnit();
+    
+    //Set the index of the SubtitleUnit within a larger set of
+    //SubtitleUnits
+    void setIndex(int i);
+    int getIndex();
+    
+    //Set and get start and end times
+    void setStartTime(long millis);
+    long getStartTime();    
+    
+    void setEndTime(long millis);
+    long getEndTime();
+    
+    //Give the SubtitleUnit titles to store for access later
+    bool setTitles(vector<ofUTF8String> words);
+    bool addTitle(ofUTF8String word);
+    
+    //Remove the ith title from the SubtitleUnit - the removed
+    //string is returned upon success
+    UTF8String removeTitle(int i);
+    
+    //Get every title contained in the SubtitleUnit
+    vector<ofUTF8String> getText();
+    
+    void print(); //Debug function
+    
+protected:
+    int index;
+    long startTime;
+    long endTime;
+    vector<ofUTF8String> text; //Size limited to max of 2
+}
+
 
 class ofxSubtitles {
-    public:
-        ofxSubtitles(string subPath, string fontPath, int fps);
-        ~ofxSubtitles();
+
+public:
+    ofxSubtitles(string subPath, string fontPath, int fps);
+    ~ofxSubtitles();
     
-        bool subsLoaded;
-        bool fontLoaded;
+    bool subsLoaded;
     
-        bool loadSubs(string path); //Constructs subtitle vector 
-        bool loadFont(string path, int fontsize); //Simple font load
-        bool loadFont(string path, int fontsize, bool antiAliased, bool fullCharacterSet, 
-                  bool makeContours, bool simplifyAmt, int dpi); //More detailed font load
+    bool loadSubs(string path); //Constructs subtitle vector 
+//  bool loadFont(string path, int fontsize); //Simple font load
+//  bool loadFont(string path, int fontsize, bool antiAliased, bool fullCharacterSet, 
+//                bool makeContours, bool simplifyAmt, int dpi); //More detailed font load
     
-        void setFPS(int fps); //Timecode's default fps is 30
-        void setTime(long milliseconds);
-        void setTime(float seconds);
-        void setTime(int frames);
-        void setFadeInterval(long milliseconds);
+    void setFramesPerSecond(int fps); //Timecode's default fps is 30
+    void setTime(long milliseconds);
+    void setTime(float seconds);
+    void setTime(int frames);
+    void setFadeInterval(long milliseconds);
     
-        //Creates a basic, non-bolded/italicized/underlined subtitle
-        //void insertSubtitle(string startTime, string endTime, ofUTF8String subtitleText);
-        //Allows you control over bolding, italics, and underlines
-        //void insertSubtitle(string startTime, string endTime, ofUTF8String subtitleText, bool bold, bool ital, bool underl);
-        //void removeSubtitle(int subtitleNumber); 
-        void draw(ofPoint p);
+    //Creates a basic, non-bolded/italicized/underlined subtitle
+    //void insertSubtitle(string startTime, string endTime, ofUTF8String subtitleText);
+    //Allows you control over bolding, italics, and underlines
+    //void insertSubtitle(string startTime, string endTime, ofUTF8String subtitleText, bool bold, bool ital, bool underl);
+    //void removeSubtitle(int subtitleNumber); 
+    void draw(ofPoint p);
+    void draw(int x, int y);
     
-        vector<Subtitle> subs;
+    ofTrueTypeFont font;
     
-    protected:
-    
-//        typedef struct AppearanceMarkers{
-//            ofColor color;
-//            bool bold;
-//            bool italics;
-//            bool underlined;
-//        }Appearance;
-                             
-        typedef struct SubtitleUnit{
-            int index; //Within the subs vector, essentially the number given for the sub in the .srt,
-                       //assuming you don't insert another subtitle independently of the file you give
-            long startTime; //In milliseconds
-            long endTime; //In milliseconds
-            //Appearance lineTypography[2]; //Array parallel to subtitle lines that marks if each line
-                                               //is italicized, bolded, etc.
-            vector<string> text; //I'm keeping the number of lines to 2 to avoid too much clutter on the screen
-        }Subtitle;
-    
-        ofxTimecode timecode;
-        Subtitle currentlyDisplayedSub;
-        string filepath;
-        long fadeTime;
-        float currentTime; //In milliseconds
-        ofTrueTypeFont font;
+protected:
+    vector<SubtitleUnit> subtitleList;
+    ofxTimecode timecode;
+    ofBuffer srtFile;
+    Subtitle currentlyDisplayedSub;
+    string filepath;
+    long fadeTime;
+    long currentTime; //In milliseconds
 }
 
 
